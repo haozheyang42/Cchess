bba4b59628def284ef253c407f75fb9c67239fda
Stephen Yang <haozheyang@gmail.com>
2020-10-18 17:24:26 -0600

    chess

--- chess1 - copy/checkAI.hpp
+++ chess1 - copy/checkAI.hpp
@@ -580,6 +580,16 @@ void allmove(int board[8][8], string p, vector<chess_move> &moves)
                 }
         }
     }
+    for (chess_move i: moves) {
+        if (i.special_move == 7) {
+            i.special_move = 6;
+        }
+        chess_move j = i;
+        for (int k = 3; k < 6; k++) {
+            j.special_move = k;
+            moves.push_back(j);
+        }
+    }
 }
 
 
